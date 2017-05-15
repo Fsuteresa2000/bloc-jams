ef15d0de0c19e34f891ade4b879dbbf80d479562
fsuteresa2000 <fsuteresa2000@yahoo.com>
2017-05-15 11:06:07 -0500

    checkpoint-19-nextandprevious-completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'bloc-jams/assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'bloc-jams/assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ var albumDads = {
+     title: 'Be The Tornado',
+     artist: 'Dads',
+     label: '6131',
+     year: '2014',
+     albumArtUrl: 'bloc-jams/assets/images/album_covers/Dads.jpg',
+     songs: [
+         { title: 'Grand Edge, MI', duration: '4:45' },
+         { title: 'Chewing Ghosts', duration: '3;35' },
+         { title: 'The Romantic Ocean', duration: '2;43'},
+         { title: 'Fake Knees', duration: '4;35' },
+         { title: 'Solid Year', duration: '4:01'}
+     ]
+ };
